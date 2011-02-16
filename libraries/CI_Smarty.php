0fd5c5c2d2574cb52b6950bfd2b6bb367a0e4e2f
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-16 16:05:54 +1000

    Smartyp was renamed to CI_Smarty. Added in assign by reference for Codeigniter object.

--- libraries/CI_Smarty.php
+++ libraries/CI_Smarty.php
@@ -2,17 +2,17 @@
 
 require_once APPPATH."third_party/Smarty/Smarty.class.php";
 
-class Smartyp extends Smarty
+class CI_Smarty extends Smarty
 {
     public function __construct()
     {
         parent::__construct();
-        
+
         $this->template_dir = APPPATH."views/";                     // Where your templates are to be loaded from
         $this->compile_dir  = BASEPATH."cache/smarty/compiled";     // Where templates are compiled
         $this->cache_dir    = APPPATH."cache/smarty/cached";        // Where templates are cached
         $this->config_dir   = APPPATH."third_party/Smarty/configs"; // Where Smarty configs are located
-        
+
         // Add all helpers to plugins_dir
         $helpers = glob(APPPATH . 'helpers/*', GLOB_ONLYDIR | GLOB_MARK);
 
@@ -20,17 +20,23 @@ class Smartyp extends Smarty
         {
             $this->plugins_dir[] = $helper;
         }
-        
+
+        if ( method_exists( $this, 'assignByRef') )
+        {
+            $ci =& get_instance();
+            $this->assignByRef("ci", $ci);
+        }
+
     }
 
     public function _assign_variables($variables = array())
     {
-        foreach ($variables as $name => $val) 
+        foreach ($variables as $name => $val)
         {
             $this->assign($name, $val);
         }
     }
-    
+
     public function set_template_dir($directory)
     {
         $this->template_dir = $directory;
