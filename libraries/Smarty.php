dfdf9a29a3b9d26401f735df62b533d2a72f4243
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-02-24 12:04:25 +1000

    Disable the exception handler in Smarty to hopefully stop exception errors being thrown.

--- libraries/Smarty.php
+++ libraries/Smarty.php
@@ -3,23 +3,24 @@
 require_once APPPATH."third_party/Smarty/Smarty.class.php";
 
 class CI_Smarty extends Smarty {
-    
+
     // Codeigniter instance
     protected $CI;
-    
+
     public function __construct()
     {
         parent::__construct();
-        
+
         // Store the Codeigniter super global instance... whatever
         $this->CI =& get_instance();
-        
+
         $this->CI->load->config('smarty');
 
-        $this->template_dir = $this->CI->config->item('template_directory');
-        $this->compile_dir  = $this->CI->config->item('compile_directory');
-        $this->cache_dir    = $this->CI->config->item('cache_directory');
-        $this->config_dir   = $this->CI->config->item('config_directory'); 
+        $this->template_dir      = $this->CI->config->item('template_directory');
+        $this->compile_dir       = $this->CI->config->item('compile_directory');
+        $this->cache_dir         = $this->CI->config->item('cache_directory');
+        $this->config_dir        = $this->CI->config->item('config_directory');
+        $this->exception_handler = null;
 
         // Add all helpers to plugins_dir
         $helpers = glob(APPPATH . 'helpers/*', GLOB_ONLYDIR | GLOB_MARK);
@@ -28,7 +29,7 @@ class CI_Smarty extends Smarty {
         {
             $this->plugins_dir[] = $helper;
         }
-        
+
         // Completely experimental, not even sure this will work
         if ( method_exists( $this, 'assignByRef') )
         {
