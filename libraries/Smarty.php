66f3465f5c4c37aa0e0888e142e70a85fbc71fe2
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-05-10 07:41:42 +1000

    There was no reason to have a CI instance on a controller level, so changed it to be assigned to a variable in the constructor. Simplified config item calls as well.

--- libraries/Smarty.php
+++ libraries/Smarty.php
@@ -14,28 +14,25 @@ require_once APPPATH."third_party/Smarty/Smarty.class.php";
 
 class CI_Smarty extends Smarty {
 
-    // Codeigniter instance
-    protected $CI;
-
     public function __construct()
     {
         parent::__construct();
 
         // Store the Codeigniter super global instance... whatever
-        $this->CI = get_instance();
+        $CI = get_instance();
 
-        $this->CI->load->config('smarty');
+        $CI->load->config('smarty');
 
-        $this->template_dir      = $this->CI->config->item('template_directory');
-        $this->compile_dir       = $this->CI->config->item('compile_directory');
-        $this->cache_dir         = $this->CI->config->item('cache_directory');
-        $this->config_dir        = $this->CI->config->item('config_directory');
-        $this->template_ext      = $this->CI->config->item('template_ext');
+        $this->template_dir      = config_item('template_directory');
+        $this->compile_dir       = config_item('compile_directory');
+        $this->cache_dir         = config_item('cache_directory');
+        $this->config_dir        = config_item('config_directory');
+        $this->template_ext      = config_item('template_ext');
         $this->exception_handler = null;
         
         // Only show serious errors. Without this if you try and use variables that
         // do not exist, Smarty will throw variable does not exist errors
-        $this->error_reporting   = $this->CI->config->item('error_reporting');
+        $this->error_reporting   = config_item('error_reporting');
 
         // Add all helpers to plugins_dir
         $helpers = glob(APPPATH . 'helpers/', GLOB_ONLYDIR | GLOB_MARK);
@@ -46,18 +43,7 @@ class CI_Smarty extends Smarty {
         }
         
         // Should let us access Codeigniter stuff in views
-        $this->assign("this", $this->CI);
-
-    }
-    
-    /**
-     * Smarty resource accessor functions
-     */
-    public function ci_get_template ($tpl_name, &$tpl_source, &$smarty_obj)
-    {
-        // ask CI to fetch our template
-        $tpl_source = $this->CI->load->view($tpl_name, $smarty_obj->get_template_vars(), true);
-        return true;
+        $this->assign("this", $CI);
     }
 
 }
