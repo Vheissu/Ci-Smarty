2054d71266d7244d106f930a119c0141ba0a8b92
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-28 15:43:03 +1000

    Added in licence info.

--- helpers/parser_helper.php
+++ helpers/parser_helper.php
@@ -1,6 +1,19 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
+ * CI Smarty
+ *
+ * Smarty templating for Codeigniter
+ *
+ * @package   CI Smarty
+ * @author    Dwayne Charrington
+ * @copyright Copyright (c) 2012 Dwayne Charrington and Github contributors
+ * @link      http://ilikekillnerds.com
+ * @license   http://www.apache.org/licenses/LICENSE-2.0.html
+ * @version   2.0
+ */
+
+/**
  * Theme URL
  *
  * A helper function for getting the current theme URL
