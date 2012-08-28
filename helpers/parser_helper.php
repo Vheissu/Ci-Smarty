f9b85f9bd372bfe08a59bf14b940232abab5c275
Dwayne Charrington <dwaynecharrington@gmail.com>
2012-08-28 15:42:23 +1000

    Documenting the helper functions for consistency.

--- helpers/parser_helper.php
+++ helpers/parser_helper.php
@@ -1,5 +1,14 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
+/**
+ * Theme URL
+ *
+ * A helper function for getting the current theme URL
+ * in a web friendly format.
+ *
+ * @param string $location
+ * @return mixed
+ */
 function theme_url($location = '')
 {
     $CI =& get_instance();
@@ -7,6 +16,14 @@ function theme_url($location = '')
     return $CI->parser->theme_url($location);
 }
 
+/**
+ * CSS
+ *
+ * A helper function for getting the current theme CSS embed code
+ * in a web friendly format
+ *
+ * @param $file
+ */
 function css($file)
 {
     $CI =& get_instance();
@@ -14,6 +31,14 @@ function css($file)
     echo $CI->parser->css($file);
 }
 
+/**
+ * JS
+ *
+ * A helper function for getting the current theme JS embed code
+ * in a web friendly format
+ *
+ * @param $file
+ */
 function js($file)
 {
     $CI =& get_instance();
@@ -21,6 +46,14 @@ function js($file)
     echo $CI->parser->js($file);
 }
 
+/**
+ * IMG
+ *
+ * A helper function for getting the current theme IMG embed code
+ * in a web friendly format
+ *
+ * @param $file
+ */
 function img($file)
 {
     $CI =& get_instance();
