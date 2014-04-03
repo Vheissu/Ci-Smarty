84adc63545f880599398e8af22fec14945bee6f8
Dwayne Charrington <dwayne@josephmark.com.au>
2014-04-03 13:55:21 +1000

    Tidy up config setting spacings.

--- config/smarty.php
+++ config/smarty.php
@@ -5,16 +5,16 @@
  *
  * Smarty templating for Codeigniter
  *
- * @package   CI Smarty
+ * @package    CI Smarty
  * @author       Dwayne Charrington
- * @copyright  2013 Dwayne Charrington and Github contributors
- * @link            http://ilikekillnerds.com
+ * @copyright  2014 Dwayne Charrington and Github contributors
+ * @link           http://ilikekillnerds.com
  * @license     http://www.apache.org/licenses/LICENSE-2.0.html
  * @version     2.0
  */
 
 // Smarty caching enabled by default unless explicitly set to FALSE
-$config['cache_status']         = TRUE;
+$config['cache_status'] = TRUE;
 
 // The path to the themes
 // Default is implied root directory/themes/
@@ -24,19 +24,19 @@ $config['theme_path'] = 'themes/';
 $config['theme_name'] = "default";
 
 // Cache lifetime. Default value is 3600 seconds (1 hour) Smarty's default value
-$config['cache_lifetime']       = 3600;
+$config['cache_lifetime'] = 3600;
 
 // Where templates are compiled
-$config['compile_directory']    = APPPATH."cache/smarty/compiled/";
+$config['compile_directory'] = APPPATH."cache/smarty/compiled/";
 
 // Where templates are cached
-$config['cache_directory']      = APPPATH."cache/smarty/cached/";
+$config['cache_directory'] = APPPATH."cache/smarty/cached/";
 
 // Where Smarty configs are located
-$config['config_directory']     = APPPATH."third_party/Smarty/configs/";
+$config['config_directory'] = APPPATH."third_party/Smarty/configs/";
 
 // Default extension of templates if one isn't supplied
-$config['template_ext']         = 'tpl';
+$config['template_ext'] = 'tpl';
 
 // Error reporting level to use while processing templates
 $config['template_error_reporting'] = E_ALL & ~E_NOTICE;
