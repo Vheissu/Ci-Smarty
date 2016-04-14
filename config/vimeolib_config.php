2a451c0798ca41903718b1ace870d4e3e1d5cf8e
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-04-14 16:22:34 +1000

    Remove the junk added in an existing PR, sorry.

--- config/vimeolib_config.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-//vimeo
-$config['vimeo']['client_id'] = 'xx';
-$config['vimeo']['client_secret'] = 'xx';
-$config['vimeo']['access_token'] = 'xx';
