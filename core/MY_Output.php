66b0c65ee867efad066e82f56309a74676bafd44
Dwayne Charrington <dwaynecharrington@gmail.com>
2017-06-30 21:55:11 +1000

    enhancement(output): remove static instance code

--- core/MY_Output.php
+++ core/MY_Output.php
@@ -5,16 +5,15 @@
  *
  * Smarty templating for Codeigniter
  *
- * Responsible for sending debug Smarty final output to browser (Smarty_Internal_Debug::display_debug) using debug console (pop-window)
- *(tks for Redn0x - http://www.smarty.net/docs/en/chapter.debugging.console.tpl)
+ * Responsible for sending debug Smarty final output to browser
  *
- * @package   CI Smarty
+ * @package    CI Smarty
  * @subpackage Core
- * @author    Dwayne Charrington
- * @copyright 2015 Dwayne Charrington and Github contributors
- * @link      http://ilikekillnerds.com
- * @license   MIT
- * @version   3.0
+ * @author     Dwayne Charrington
+ * @copyright  2017 Dwayne Charrington and Github contributors
+ * @link       http://ilikekillnerds.com
+ * @license    MIT
+ * @version    3.0
  */
 
 
@@ -23,10 +22,7 @@ class MY_Output extends CI_Output {
 	public function _display($output = '') {
 	    parent::_display($output);
 
-  		// If Smarty is active - NOTE: $this->output->enable_profiler(TRUE) active Smarty debug to simplify
-  		if (class_exists('CI_Controller') && class_exists('Smarty_Internal_Debug') && (config_item('smarty_debug') || $this->enable_profiler)) {
-  			  $CI =& get_instance();
-          Smarty_Internal_Debug::display_debug( $CI->smarty);
-  		}
+  		$CI =& get_instance();
+		$CI->smarty->_debug->display_debug($CI->smarty, TRUE);
 	}
 }
